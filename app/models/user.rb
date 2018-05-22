7174499fbaf44f2b255c30b92ee2b781643bfd3d
OliverPerret <oliver.perret2@gmail.com>
2018-05-22 16:03:27 +0200

    user subscription

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :bookings_as_renter, class_name: "Booking"
   has_many :bookings_as_owner, through: :places, source: :bookings
 
-  validates :name, presence: true, length: {minimum: 3}
+  # validates :name, presence: true, length: {minimum: 3}
   # validates :phone_number, presence: true
   # # validates :email, presence: true, uniqueness: true, format: { with: /\A.*@.*\.com\z/ }
   # validates :age, presence: true
