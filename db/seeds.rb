09e965ae90a6afd8426934253f69cfe2843463e1
Maxime Rimbault <maxime.rimbault@teksi.net>
2018-05-22 16:05:33 +0200

    Working on index view of places

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "maxime@rimbault.fr", encrypted_password: "lalala", name: "Robert")
+
+Place.create(title: "Tati shop", address: "14 avenue Napoléon, PARIS 75012", description: "nice", price: 42, equipment: "lot of stuff", destroy_pack: "Damage", category: "Shop", rate: 4, sleepings: 4, tools: "Shop")
+Place.create(title: "Garage", destroy_pack: "Destroy", price: 39, category: "Shop")
+Place.create(title: "Villa", destroy_pack: "Damage", price: 45, category: "House & apt.")
+Place.create(title: "Hotel reception", destroy_pack: "Damage", price: 31, category: "Tourism")
+Place.create(title: "School class", destroy_pack: "Damage", price: 22, category: "Other")
+Place.create(title: "Manor", price: 76, category: "Luxury")
+
