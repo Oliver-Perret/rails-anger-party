44109d25d34a8438c2db78fa1b0bf7689f85f53a
Maxime Rimbault <maxime.rimbault@teksi.net>
2018-05-24 13:22:37 +0200

    search bar done and css button improvements

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,4 @@ Place.create(title: "Luxury villa in Spain", owner: malika, address: "14 avenue 
 Place.create(title: "Cozy Alpes chalet", owner: malika, address: "14 avenue Napoléon, PARIS 75012", description: "A nice and cosy clothes shop in the heart of Paris", price: 122, equipment: "lot of stuff", destroy_pack: "Damage", category: "Shop", rate: 4, sleepings: 12, tools: "Flame thrower")
 Place.create(title: "Old garage", owner: pierre, address: "14 avenue Napoléon, PARIS 75012", description: "A nice and cosy clothes shop in the heart of Paris", price: 235, equipment: "lot of stuff", destroy_pack: "Damage", category: "Shop", rate: 4, sleepings: 12, tools: "Flame thrower")
 Place.create(title: "Suhi restaurant", owner: pierre, address: "14 avenue Napoléon, PARIS 75012", description: "A nice and cosy clothes shop in the heart of Paris", price: 750, equipment: "lot of stuff", destroy_pack: "Damage", category: "Shop", rate: 4, sleepings: 12, tools: "Flame thrower")
+
