e1ef3488049435dbfb348205bb1d868c5f0be72a
raphael de seroux <rdeseroux@gmail.com>
2018-05-25 14:47:57 +0200

    adding exception to authentification

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: :home, only: :index
   def home
+  end
+
+  def index
   end
 
   def dashboard
