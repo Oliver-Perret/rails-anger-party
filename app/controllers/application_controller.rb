36c1982a4e399713a883ddf8271f59f339e76fbd
raphael de seroux <rdeseroux@gmail.com>
2018-05-25 15:15:28 +0200

    added avatar picture, ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
   # Pundit: white-list approach.
@@ -14,6 +15,15 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def configure_permitted_parameters
+     # For additional fields in app/views/devise/registrations/new.html.erb
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :email, :password, :avatar])
+
+     # For additional in app/views/devise/registrations/edit.html.erb
+     devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :email, :password, :avatar])
+   end
+
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
